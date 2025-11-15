5358057df29f8f1cc7133c1c4439c57b2072272d
sanghyup <hyupsang.dev@gmail.com>
2025-11-15 09:38:48 +0900

    feat: initialize Spring Boot project using Spring Initializr
    스프링 부트를 기술 스택으로 백엔드 구조 생성

---
+++ backend/src/test/java/com/todaysus/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaysus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
