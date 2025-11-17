1e0353cb8b27d06b7ba5e7ce516552776396ef3c
sanghyup <hyupsang.dev@gmail.com>
2025-11-18 03:03:38 +0900

    refactor: healthController를 HealthController로 이름 변경

--- backend/src/main/java/com/todaysus/backend/health/HealthController.java
+++ backend/src/main/java/com/todaysus/backend/health/HealthController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class healthController {
+public class HealthController {
 
     @GetMapping("/health")
     public String health() {
